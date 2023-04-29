ec28539ba2d909b7281b6821de3b880a73c69e3c
Van Nguyen <nnphuongvan@gmail.com>
2023-04-29 16:15:30 +0700

    add Header to views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230426080373) do
+ActiveRecord::Schema.define(version: 20230426091233) do
 
   create_table "refinery_authentication_devise_roles", force: :cascade do |t|
     t.string "title"
@@ -49,6 +49,78 @@ ActiveRecord::Schema.define(version: 20230426080373) do
     t.string "full_name"
     t.index ["id"], name: "index_refinery_authentication_devise_users_on_id"
     t.index ["slug"], name: "index_refinery_authentication_devise_users_on_slug"
+  end
+
+  create_table "refinery_blog_categories", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "slug"
+    t.index ["id"], name: "index_refinery_blog_categories_on_id"
+    t.index ["slug"], name: "index_refinery_blog_categories_on_slug"
+  end
+
+  create_table "refinery_blog_categories_blog_posts", force: :cascade do |t|
+    t.integer "blog_category_id"
+    t.integer "blog_post_id"
+    t.index ["blog_category_id", "blog_post_id"], name: "index_blog_categories_blog_posts_on_bc_and_bp"
+  end
+
+  create_table "refinery_blog_category_translations", force: :cascade do |t|
+    t.integer "refinery_blog_category_id", null: false
+    t.string "locale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "slug"
+    t.index ["locale"], name: "index_refinery_blog_category_translations_on_locale"
+    t.index ["refinery_blog_category_id"], name: "index_a0315945e6213bbe0610724da0ee2de681b77c31"
+  end
+
+  create_table "refinery_blog_comments", force: :cascade do |t|
+    t.integer "blog_post_id"
+    t.boolean "spam"
+    t.string "name"
+    t.string "email"
+    t.text "body"
+    t.string "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["blog_post_id"], name: "index_refinery_blog_comments_on_blog_post_id"
+    t.index ["id"], name: "index_refinery_blog_comments_on_id"
+  end
+
+  create_table "refinery_blog_post_translations", force: :cascade do |t|
+    t.integer "refinery_blog_post_id", null: false
+    t.string "locale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "body"
+    t.text "custom_teaser"
+    t.string "custom_url"
+    t.string "slug"
+    t.string "title"
+    t.index ["locale"], name: "index_refinery_blog_post_translations_on_locale"
+    t.index ["refinery_blog_post_id"], name: "index_refinery_blog_post_translations_on_refinery_blog_post_id"
+  end
+
+  create_table "refinery_blog_posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "draft"
+    t.datetime "published_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "user_id"
+    t.string "custom_url"
+    t.text "custom_teaser"
+    t.string "source_url"
+    t.string "source_url_title"
+    t.integer "access_count", default: 0
+    t.string "slug"
+    t.string "username"
+    t.index ["access_count"], name: "index_refinery_blog_posts_on_access_count"
+    t.index ["id"], name: "index_refinery_blog_posts_on_id"
+    t.index ["slug"], name: "index_refinery_blog_posts_on_slug"
   end
 
   create_table "refinery_image_translations", force: :cascade do |t|
@@ -151,6 +223,20 @@ ActiveRecord::Schema.define(version: 20230426080373) do
     t.datetime "updated_at"
   end
 
+  create_table "refinery_settings", force: :cascade do |t|
+    t.string "name"
+    t.text "value"
+    t.boolean "destroyable", default: true
+    t.string "scoping"
+    t.boolean "restricted", default: false
+    t.string "form_value_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "slug"
+    t.string "title"
+    t.index ["name"], name: "index_refinery_settings_on_name"
+  end
+
   create_table "seo_meta", force: :cascade do |t|
     t.integer "seo_meta_id"
     t.string "seo_meta_type"
@@ -160,6 +246,23 @@ ActiveRecord::Schema.define(version: 20230426080373) do
     t.datetime "updated_at", null: false
     t.index ["id"], name: "index_seo_meta_on_id"
     t.index ["seo_meta_id", "seo_meta_type"], name: "id_type_index_on_seo_meta"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context"
+    t.datetime "created_at"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
 end
